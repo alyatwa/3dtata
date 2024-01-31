0390f0bdf5fb299c5e549dcbeba3cda073e40ebb
ali atwa <ali.atwa2040@gmail.com>
2024-01-31 23:49:18 +0200

    build

--- next.config.js
+++ next.config.js
@@ -27,7 +27,7 @@ const nextConfig = {
   images: { unoptimized: true },
   pageExtensions: ["jsx", "tsx", "ts"],
   output: undefined, // Outputs a Single-Page Application (SPA)
-  distDir: "./dist", // Changes the output directory `./dist/`
+  //distDir: "./dist", // Changes the output directory `./dist/`
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   typescript: {
     ignoreBuildErrors: true,
